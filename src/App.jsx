5bf5f0b2c0be071f9eb177ff970b7a166f5d1053
Harshith R <harshith.raju0710@gmail.com>
2024-09-04 20:33:50 +0530

    trying reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Card from "./components"
+import {useState} from 'react'
+
 function App() {
+  const [from, setFrom] = useState('USD');
+  const [to, setTo] = useState('INR');
   return (
     <>
      <h1 className="text-center font-semibold">Currency Converter</h1>
+     <Card title='From' currencyType={from}/>
+     <Card title='to' currencyType={to}/>
     </>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+const Card = ({title, currencyType}) => {
+    
+  return (
+    <>
+        <div>{title}</div>
+        <div>{currencyType}</div>
+    </>
+  )
+}
+
+export default Card
